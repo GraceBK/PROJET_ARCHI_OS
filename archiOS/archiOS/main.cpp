482318412df6b4d79d7f480242b5375e0d991c05
Grace BK <iris.b1991@gmail.com>
2017-12-30 11:45:24 +0100

    Initialisation des Sources

---
+++ archiOS/archiOS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  archiOS
+//
+//  Created by Grace on 30/12/2017.
+//  Copyright © 2017 Grace BOUKOU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Projet Archi-OS!\n";
+    return 0;
+}
