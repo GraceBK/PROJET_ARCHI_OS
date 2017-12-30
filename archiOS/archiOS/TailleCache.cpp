7fbb5db19a249c744e6ab7a43f2a9fd967e8251d
Grace BK <iris.b1991@gmail.com>
2017-12-30 11:48:32 +0100

    Init TailleCache
    Création des Fichiers (c++ et h)

---
+++ archiOS/archiOS/TailleCache.cpp
@@ -0,0 +1,9 @@
+//
+//  TailleCache.cpp
+//  archiOS
+//
+//  Created by Grace on 30/12/2017.
+//  Copyright © 2017 Grace BOUKOU. All rights reserved.
+//
+
+#include "TailleCache.hpp"
---
+++ archiOS/archiOS/TailleCache.hpp
@@ -0,0 +1,14 @@
+//
+//  TailleCache.hpp
+//  archiOS
+//
+//  Created by Grace on 30/12/2017.
+//  Copyright © 2017 Grace BOUKOU. All rights reserved.
+//
+
+#ifndef TailleCache_hpp
+#define TailleCache_hpp
+
+#include <stdio.h>
+
+#endif /* TailleCache_hpp */
